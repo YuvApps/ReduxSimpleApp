ad7878a31ad3a4320f555f231db6b40475aecacc
Yuval Aharoni <yuvalaharoni585@gmail.com>
2018-09-28 13:54:21 +0300

    Initial Commit + books reducer adding

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Secret' },
+    { title: 'Sapiens: A Brief History of Humankind' }
+  ]
+}
