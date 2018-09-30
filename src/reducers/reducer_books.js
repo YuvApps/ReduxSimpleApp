96f181e6e7540e1d63406a5a9d27f7291c5ac5ee
Yuval Aharoni <yuvalaharoni585@gmail.com>
2018-10-01 00:31:24 +0300

    Final Version

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,20 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Secret' },
-    { title: 'Sapiens: A Brief History of Humankind' }
+    {
+      title: 'Javascript: The Good Parts',
+      pages: 98
+    },
+    {
+      title: 'Harry Potter',
+      pages: 156
+    },
+    {
+      title: 'The Secret',
+      pages: 320
+    },
+    {
+      title: 'Sapiens: A Brief History of Humankind',
+      pages: 452
+    }
   ]
 }
